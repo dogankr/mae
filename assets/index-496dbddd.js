db2ae80524439b082634a8fe0ac8f541ed8a643b
methral <a@a.comm>
2023-09-22 17:26:11 +0300

    init

---
+++ assets/index-496dbddd.js
@@ -0,0 +1,3 @@
+import{u as c,b as i,c as o,r as n,j as s,O as l,s as p}from"./index-fc75dee1.js";const v=()=>{const a=c(),t=i(),[r,m]=o();console.log("Predictive / params:",t),console.log("Predictive / sp:",r);const e=["predictive","similarMatters"].includes(t.type);return n.useEffect(()=>{e||a("/")},[]),console.log("Analytics / typeCheck:",e),e?s.jsx(u,{children:s.jsx(l,{})}):s.jsx(s.Fragment,{})},u=p.div`
+
+`;export{v as default};
